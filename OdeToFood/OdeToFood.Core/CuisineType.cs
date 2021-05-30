39b2942e1362a4333225034e0cf220ea54f6e902
nguyenngoccuong16122202 <nguyenngoccuong.16122002@hotmail.com>
2021-05-30 11:51:19 +0700

    commit code

---
+++ OdeToFood/OdeToFood.Core/CuisineType.cs
@@ -0,0 +1,11 @@
+﻿
+namespace OdeToFood.Core
+{
+    public enum CuisineType
+    {
+        none,
+        Mexican,
+        Italian,
+        Indian
+    }
+}
---
+++ OdeToFood/OdeToFood.Data/RestaurantData.cs
@@ -0,0 +1,71 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id=1,Name="abc",Location="x",Cuisine=CuisineType.Indian },
+                new Restaurant{Id=2,Name="def",Location="y",Cuisine=CuisineType.Italian },
+                new Restaurant{Id=3,Name="xyz",Location="z",Cuisine=CuisineType.Mexican},
+            };
+        }
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+
+        }
+        public Restaurant Update(Restaurant updateRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updateRestaurant.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updateRestaurant.Name;
+                restaurant.Location = updateRestaurant.Location;
+                restaurant.Cuisine = updateRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+        public int Commit()
+        {
+            return 0;
+        }
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = restaurants.FirstOrDefault(r => r.Id == id);
+            if (restaurant != null)
+            {
+                restaurants.Remove(restaurant);
+            }
+            return restaurant;
+        }
+
+        public int GetCountOfRestaunts()
+        {
+            return restaurants.Count();
+        }
+    }
+}
